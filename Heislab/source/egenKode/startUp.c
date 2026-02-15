d7d9836ce10d04a438c66e39d94cfe87e72140b4
JorandRunningenNTNU <jorandru@stud.ntnu.no>
2026-02-15 13:38:24 +0100

    Ferdig med startUp.c, men ikke testet, bør testes fysisk

---
+++ Heislab/source/egenKode/startUp.c
@@ -0,0 +1,17 @@
+#include "startUp.h"
+
+MotorDirection direction = DIRN_DOWN;
+int floor = -1;
+
+int startUpUpdate(){
+    floor = elevio_floorSensor();
+    if (floor != -1){
+        direction = DIRN_STOP;
+        return 1;
+    }
+    return 0;
+}
+
+MotorDirection startUpDirection(){
+    return direction;
+}
