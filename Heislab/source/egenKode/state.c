556bc10b1dbb04d3e70a3ac46f0c02d44853ce7b
JorandRunningenNTNU <jorandru@stud.ntnu.no>
2026-02-16 09:50:57 +0100

    Begynt på state.c

---
+++ Heislab/source/egenKode/state.c
@@ -0,0 +1,12 @@
+#include "state.h"
+#include "input.h"
+
+void stateUpdate(){
+    if (inputStopp());
+    
+    int Etasje = inputFloorSensor();
+    if (Etasje = -1){
+
+    }
+    return Etasje;
+}
